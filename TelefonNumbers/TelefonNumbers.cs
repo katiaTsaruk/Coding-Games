d6af1d4142f8cc0d49ca7c6e1c52bf522a3ab305
Katia <katrusia.tsaruk@gmail.com>
2021-09-25 17:44:53 +0300

    telef progress

--- TelefonNumbers/TelefonNumbers.cs
+++ TelefonNumbers/TelefonNumbers.cs
@@ -1,18 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Coding_Games
 {
     public class PhoneNumbers
     {
+        static List<List<int>> telephones = new List<List<int>>();
         public static void Main(string[] args)
         {
+            telephones = GetNumbers();
+        }
+
+        static List<List<int>> GetNumbers()
+        {
+            List<List<int>> listLists = new List<List<int>>();
+            List<int> list = new List<int>();
             int n = int.Parse(Console.ReadLine());
             for (int i = 0; i < n; i++)
             {
                 string telephone = Console.ReadLine();
+                for (int j = 0; j < telephone.Length; j++)
+                {
+                    int number = int.Parse(telephone.Substring(i, i + 1));
+                    list.Add(number);
+                }
+
+                listLists.Add(list);
             }
-        
-            Console.WriteLine("number");
+            return listLists;
+        }
+
+        static void SystemiseData()
+        {
+            
         }
     }
 }
